a6dadbf183c5ed29396e48ba09c35eec03e748b1
Camilo Riveros <camilor2611@gmail.com>
2021-07-06 14:19:14 +0000

    Add function

--- tasks.py
+++ tasks.py
@@ -1,4 +1,7 @@
 from RPA.Browser.Selenium import Selenium
+#from RPA.Outlook.Application import Application
+from RPA.Desktop import Desktop
+desktop = Desktop()
 
 browser = Selenium()
 url = "https://robocorp.com/docs/"
@@ -19,12 +22,34 @@ def search_for(term: str):
 def store_screenshot(filename: str):
     browser.screenshot(filename=filename)
 
+
+def write_entry_in_accounting(entry):
+    desktop.open_application("C:\Program Files\DBeaver\dbeaver.exe")
+    desktop.click(f"image:{ROBOT_ROOT}/images/create.png")
+    desktop.type_text(entry)
+    desktop.press_keys("ctrl", "s")
+    desktop.press_keys("enter")
+
+
+from RPA.Excel.Application import Application
+
+def excel_rpa():
+    app = Application()
+    app.open_application()
+    app.open_workbook('input/Prueba.xlsx')
+    app.set_active_worksheet(sheetname='Hoja')
+    app.write_to_cells(row=1, column=1, value='new data')
+    app.save_excel()
+    app.quit_application()
+
 # Define a main() function that calls the other functions in order:
 def main():
     try:
         open_the_website(url)
         search_for(term)
         store_screenshot(screenshot_filename)
+        excel_rpa()
+        #write_entry_in_accounting('s')
     finally:
         browser.close_all_browsers()
 
